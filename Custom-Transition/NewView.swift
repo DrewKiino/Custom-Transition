4941c3a788fc31274d0ede143ceeb3534d072dca
drewkiino <andrewaquino118@yahoo.com>
2017-07-28 19:06:38 -0700

    moved transition logic to navigation controller, used push/pop for transitions

--- Custom-Transition/NewView.swift
+++ Custom-Transition/NewView.swift
@@ -8,51 +8,51 @@
 
 import UIKit
 
-class NewView: UIViewController, UIViewControllerTransitioningDelegate {
-
+class NewView: UIViewController {
+  
+  deinit {
+    print("deinit")
+  }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
     
-    // Initialize class variables
-    let customAnimationPresenter = CustomAnimationPresenter()
-    let customAnimationDismisser = CustomAnimationDismisser()
-
+    print("view loaded")
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        // Call delegate to viewcontroller for animation
-        self.transitioningDelegate = self
-        
-        
-        
-        // Set UI
-        self.view.backgroundColor = .gray
-        navigationController?.isNavigationBarHidden = true
-        
-        
-        
-        // Custom Button
-        let btn: UIButton = UIButton(frame: CGRect(x: 100, y: 400, width: 100, height: 50))
-        btn.backgroundColor = UIColor.darkGray
-        btn.setTitle("Back", for: .normal)
-        btn.addTarget(self, action: #selector(NewView.popBack), for: .touchUpInside)
-        btn.tag = 1
-        self.view.addSubview(btn)
-        self.view.backgroundColor = UIColor.white
-        
-    }
+    // Set UI
+    self.view.backgroundColor = .gray
+    navigationController?.isNavigationBarHidden = true
     
-    // MARK: Animation Protocols conforming to Delegate
-    func animationController(forPresented presented: UIViewController, presenting: UIViewController, source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
-        return customAnimationPresenter
-    }
+    // Custom Button
+    let btn: UIButton = UIButton(frame: CGRect(x: 100, y: 400, width: 100, height: 50))
+    btn.backgroundColor = UIColor.darkGray
+    btn.setTitle("Back", for: .normal)
+    btn.addTarget(self, action: #selector(self.popBack), for: .touchUpInside)
+    btn.tag = 1
+    self.view.addSubview(btn)
+    self.view.backgroundColor = UIColor.white
     
-    func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
-        return customAnimationDismisser
-    }
-   
-    @objc func popBack() {
-        self.dismiss(animated: true, completion: nil)
-    }
-    
-    
+    // Custom Button
+    let btn2: UIButton = UIButton(frame: CGRect(x: 100, y: 100, width: 100, height: 50))
+    btn2.backgroundColor = UIColor.darkGray
+    btn2.setTitle("Next", for: .normal)
+    btn2.addTarget(self, action: #selector(self.nextView), for: .touchUpInside)
+    btn2.tag = 1
+    self.view.addSubview(btn2)
+  }
+  
+  func popBack() {
+//     self.dismiss(animated: true, completion: nil)
+          self.navigationController?.popViewController(animated: true)
+//          self.navigationController?.popToRootViewController(animated: true)
+//    if let viewControllers = self.navigationController?.viewControllers {
+//      self.navigationController?.popToViewController(viewControllers[2], animated: true)
+//    }
+  }
+  
+  // Present New View Method
+  func nextView() {
+//        self.present(NewView(), animated: true, completion: nil)
+    self.navigationController?.pushViewController(NewView(), animated: true)
+  }
 }
